007c30fcc21c2ee860e1c62c81eb63191a81b06f
adriano.dantas <adriano.dantas@omni.com.br>
2022-03-04 18:06:33 -0300

    Login com oauth

--- src/main/java/com/algamoney/api/config/security/CustomSecurityConfig.java
+++ src/main/java/com/algamoney/api/config/security/CustomSecurityConfig.java
@@ -2,7 +2,9 @@ package com.algamoney.api.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -12,6 +14,17 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    /**
+     * Método utilizado para corrigir o erro de cors para Oauth2 (end point de login que gera o access token)
+     * utilizado em conjunto com o filtro CorsFilter
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/oauth/token");
     }
 }
 
