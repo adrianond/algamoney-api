812c95401600adb389d0615bf1d2df1f0825d52a
adrianond <adrianond@yahoo.com.br>
2023-12-26 17:56:20 -0300

    Swagger config - remoção end points oauth-security

--- src/main/java/com/algamoney/api/config/swagger/SwaggerConfig.java
+++ src/main/java/com/algamoney/api/config/swagger/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.algamoney.api.http"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());
